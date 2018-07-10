5d8e0585f64c8f848ea1752ab8b2161612b79ff9
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-07-10 16:43:31 +0800

    init commit - up to use of mongoose

---
+++ models/author.js
@@ -0,0 +1,40 @@
+const mongoose = require( 'mongoose' );
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema(
+{
+    first_name:
+    {
+        type: String,
+        required: true,
+        max: 100
+    },
+    family_name:
+    {
+        type: String,
+        required: true,
+        max: 100
+    },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date }
+});
+
+// virtual for author's full name
+AuthorSchema
+    .virtual( 'name' )
+    .get( function ()
+    {
+        return `${this.family_name}, ${this.first_name}`;
+    });
+
+// virtual for author's URL
+AuthorSchema
+    .virtual( 'url' )
+    .get( function ()
+    {
+        return `/catalog/author/${this._id}`;
+    });
+
+// export model
+module.exports = mongoose.model( 'Author', AuthorSchema );
---
+++ models/genre.js
@@ -0,0 +1,25 @@
+const mongoose = require( 'mongoose' );
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema(
+{
+    name:
+    {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100
+    }
+});
+
+// virtual for genre's URL
+GenreSchema
+    .virtual( 'url' )
+    .get( function ()
+    {
+        return `/catalog/genre/${this._id}`;
+    });
+
+// export model
+module.exports = mongoose.model( 'Genre', GenreSchema );
