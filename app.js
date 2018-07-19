27e76a762d09c1655ed2ff10a9feb44be862c06a
popoyMM <popoy@mm.dev>
2018-07-19 10:53:18 +0800

    added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require( 'express' );
 const path = require( 'path' );
 const cookieParser = require( 'cookie-parser' );
 const logger = require( 'morgan' );
+const compression = require( 'compression' );
+const helmet = require( 'helmet' );
 
 const indexRouter = require( './routes/index' );
 const catalogRouter = require( './routes/catalog' );
@@ -23,10 +25,13 @@ db.on( 'error', console.error.bind( console, 'MongoDB connection error:' ));
 app.set( 'views', path.join( __dirname, 'views' ));
 app.set( 'view engine', 'pug' );
 
+// middlewares
+app.use( helmet());
 app.use( logger( 'dev' ));
 app.use( express.json());
 app.use( express.urlencoded({ extended: false }));
 app.use( cookieParser());
+app.use( compression()); // compress all routes
 app.use( express.static( path.join( __dirname, 'public' )));
 
 app.use( '/', indexRouter );
